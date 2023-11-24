b9bf6dd4a20b0876ef476e8a4ed4c4a0009a39b3
jedsonSantos4 <jedson.chapa@gmail.com>
2023-11-24 09:11:21 -0300

    add service

---
+++ SFTP.Service/Interface/ISftpService.cs
@@ -0,0 +1,12 @@
+﻿using Renci.SshNet.Sftp;
+
+namespace SFTP.Service.Interface
+{
+    public interface ISftpService
+    {
+        IEnumerable<ISftpFile> ListAllFiles(string remoteDirectory = ".");
+        void UploadFile(string localFilePath, string remoteFilePath);
+        void DownloadFile(string remoteFilePath, string localFilePath);
+        void DeleteFile(string remoteFilePath);
+    }
+}
---
+++ SFTP.Service/Model/SftpConfig.cs
@@ -0,0 +1,20 @@
+﻿namespace SFTP.Service.Model
+{
+    public class SftpConfig
+    {        
+
+        public SftpConfig(string host, int port, string name, string pass)
+        {
+            this.Host = host;
+            this.Port = port;
+            this.UserName = name;
+            this.Password = pass;
+        }
+
+        public string Host { get; set; } = string.Empty;
+        public int Port { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        
+    }
+}
